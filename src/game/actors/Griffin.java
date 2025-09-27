3e8381ed1545609391b5775b268c77a37e5308a5
skho0064 <skho0064@student.monash.edu>
2025-09-27 15:03:58 +0800

    Moved the initial basic changeState logic to parent class MythicalCreature

--- src/game/actors/Griffin.java
+++ src/game/actors/Griffin.java
@@ -3,14 +3,11 @@ package game.actors;
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actions.DoNothingAction;
-import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.actors.Behaviour;
-import edu.monash.fit2099.engine.capabilities.Status;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
 import game.weapons.Claw;
 
-import java.util.ArrayList;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -21,26 +18,20 @@ public class Griffin extends MythicalCreature{
         super(name, displayChar, hitPoints);
         this.setIntrinsicWeapon(new Claw());
 
-        int statesIndexCounter = 0;
-        ArrayList<Status> states = new ArrayList<>();
-        states.add(new Angry(this));
-        states.add(new Sleepy(this));
-        states.add(new Poisonous(this));
+        // Adding all the possible states of Griffin to the list of states for changeState to loop through
+        this.statesList.add(new Angry(this));
+        this.statesList.add(new Sleepy(this));
+        this.statesList.add(new Poisonous(this));
     }
 
     @Override
     public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
+        changeState(this);
         for (Behaviour behaviour : behaviours.values()) {
             Action action = behaviour.generateAction(this, map);
             if(action != null)
                 return action;
         }
         return new DoNothingAction();
-    }
-
-
-    @Override
-    public void changeState(Actor actor) {
-
     }
 }
