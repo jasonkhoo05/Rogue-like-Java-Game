cc2a2b16e3f4b5e28f2e109fe5d28b25f58e0f82
skho0064 <skho0064@student.monash.edu>
2025-09-27 14:41:01 +0800

    Implemented playTurn() method of Griffin class

--- src/game/actors/Griffin.java
+++ src/game/actors/Griffin.java
@@ -2,29 +2,40 @@ package game.actors;
 
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actions.DoNothingAction;
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.actors.Behaviour;
 import edu.monash.fit2099.engine.capabilities.Status;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
 import game.weapons.Claw;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Griffin extends MythicalCreature{
+    public Map<Integer, Behaviour> behaviours = new TreeMap<>();
+
     public Griffin(String name, char displayChar, int hitPoints) {
         super(name, displayChar, hitPoints);
         this.setIntrinsicWeapon(new Claw());
 
         int statesIndexCounter = 0;
         ArrayList<Status> states = new ArrayList<>();
-        states.add(new Angry());
-        states.add(new Sleepy());
-        states.add(new Poisonous());
+        states.add(new Angry(this));
+        states.add(new Sleepy(this));
+        states.add(new Poisonous(this));
     }
 
     @Override
     public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
-        return null;
+        for (Behaviour behaviour : behaviours.values()) {
+            Action action = behaviour.generateAction(this, map);
+            if(action != null)
+                return action;
+        }
+        return new DoNothingAction();
     }
 
 
