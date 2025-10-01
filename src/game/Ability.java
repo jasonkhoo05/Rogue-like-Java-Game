8261f24a8dd7aed43153ffd224297498b192623d
Lee Jia Wey <jlee0482@student.monash.edu>
2025-10-01 22:14:03 +0800

    1/10/2025 Added new Ability that Unicorn and Player can have effect

--- src/game/Ability.java
+++ src/game/Ability.java
@@ -1,12 +1,19 @@
 package game;
 
+import edu.monash.fit2099.engine.capabilities.Status;
+
 /**
  * Use this enum to represent abilities.
  * Example #1: if the player can jump over walls, you can attach Abilities.WALL_JUMP to the Player class
  */
-public enum Ability {
+public enum Ability
+{
     CAN_ATTACK,
     ITEMSPAWNABLE,
     CAN_TAME,
     TAMED,
+    HEALABLE,
+    RECEIVED_HEAL,
+    IMMUNITY,
+    BOOST_DAMAGE,
 }
