416a4719aafc8bab6596fef54ba1bbbe4719527d
Lee Jia Wey <jlee0482@student.monash.edu>
2025-10-02 02:39:37 +0800

    1/10/2025 Created Unicorn class

---
+++ src/game/actors/Unicorn.java
@@ -0,0 +1,25 @@
+package game.actors;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actions.DoNothingAction;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.actors.Behaviour;
+import edu.monash.fit2099.engine.capabilities.Status;
+import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.Ability;
+import game.State.Aegis;
+import game.State.Radiant;
+import game.State.StatusType;
+import game.State.Vigor;
+import game.behaviours.WanderBehaviour;
+
+
+import java.util.Map;
+import java.util.TreeMap;
+
+
+public class Unicorn extends MythicalCreature {
+
+}
