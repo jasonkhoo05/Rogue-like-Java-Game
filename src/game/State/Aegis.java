8544731d4d1121b4e5b855dc034dea75d3904441
Lee Jia Wey <jlee0482@student.monash.edu>
2025-10-02 02:36:31 +0800

    1/10/2025 Defined functionality of Aegis which is providing immunity for player

--- src/game/State/Aegis.java
+++ src/game/State/Aegis.java
@@ -9,4 +9,32 @@ import game.Ability;
 
 public class Aegis implements Status {
 
+    private int duration;
+
+    /**
+     * Constructor for Radiant state.
+     * @param duration number of turns the state lasts
+     */
+    public Aegis(int duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public void tickStatus(GameEntity currEntity, Location location) {
+        // Apply immunity only to the entity holding this status
+        if (currEntity.hasAbility(Ability.HEALABLE)) {
+            currEntity.enableAbility(Ability.IMMUNITY);  // mark as immune this turn
+        }
+
+        duration--;
+        if (duration <= 0 && currEntity.hasAbility(Ability.IMMUNITY)) {
+            // remove immunity once duration ends
+            currEntity.disableAbility(Ability.IMMUNITY);
+        }
+    }
+
+    @Override
+    public boolean isStatusActive() {
+        return duration > 0;
+    }
 }
