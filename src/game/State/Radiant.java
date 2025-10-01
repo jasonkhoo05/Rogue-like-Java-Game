c60e9010e6c272ac79f07a5ef091a020524ba3f2
Lee Jia Wey <jlee0482@student.monash.edu>
2025-10-02 02:38:50 +0800

    1/10/2025 Defined healing functionality to nearby players who have the HEALABLE ability

--- src/game/State/Radiant.java
+++ src/game/State/Radiant.java
@@ -11,4 +11,33 @@ import game.Ability;
 
 public class Radiant implements Status {
 
+    private int duration;
+
+    /**
+     * Constructor for Radiant state.
+     * @param duration number of turns the state lasts
+     */
+    public Radiant(int duration) {
+        this.duration = duration;
+    }
+    @Override
+    public void tickStatus(GameEntity currEntity, Location location) {
+        // Heal any adjacent entities with HEALABLE ability
+        for (Exit exit : location.getExits()) {
+            Location dest = exit.getDestination();
+            if (dest.containsAnActor()) {
+                GameEntity neighbour = dest.getActor();
+
+                if (neighbour.hasAbility(Ability.HEALABLE)) {
+                    neighbour.enableAbility(Ability.RECEIVED_HEAL);
+                }
+            }
+        }
+        duration--;
+    }
+
+    @Override
+    public boolean isStatusActive() {
+        return duration > 0;
+    }
 }
