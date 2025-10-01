ba641ee4bcf973a4a67b93e9a8c6a4bee930650a
Lee Jia Wey <jlee0482@student.monash.edu>
2025-10-01 22:10:12 +0800

    1/10/2025 Created Vigor class for Unicorn

---
+++ src/game/State/Vigor.java
@@ -0,0 +1,11 @@
+package game.State;
+
+import edu.monash.fit2099.engine.GameEntity;
+import edu.monash.fit2099.engine.capabilities.Status;
+import edu.monash.fit2099.engine.positions.Exit;
+import edu.monash.fit2099.engine.positions.Location;
+import game.Ability;
+
+public class Vigor implements Status {
+
+}
