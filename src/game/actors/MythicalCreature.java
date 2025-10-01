ebae25aa9d4eb5a8aa324f94a5c381e44864343f
Lee Jia Wey <jlee0482@student.monash.edu>
2025-10-02 02:37:26 +0800

    1/10/2025 Changed variable statesList to hold StatusType

--- src/game/actors/MythicalCreature.java
+++ src/game/actors/MythicalCreature.java
@@ -2,14 +2,17 @@ package game.actors;
 
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.capabilities.Status;
+import game.State.StatusType;
 import game.capabilities.StateChangeable;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public abstract class MythicalCreature extends Actor implements StateChangeable {
     int statesIndexCounter = -1;
     // A list of all possible states of this MythicalCreature
-    ArrayList<Status> statesList;
+    List<StatusType> statesList = new ArrayList<>();
+
     /**
      * The constructor of the Actor class.
      *
@@ -25,12 +28,13 @@ public abstract class MythicalCreature extends Actor implements StateChangeable 
 
     @Override
     public void changeState(Actor actor) {
-        // Statuses automatically remove itself once it is finished
-        // So if MythicalCreature don't have any state anymore, it is time to change state
-        if (this.statuses().isEmpty()) {
-            // Loops back to initial state after all state have been transversed
-            this.statesIndexCounter = (this.statesIndexCounter + 1) % this.statesList.size();
-            this.addStatus(statesList.get(this.statesIndexCounter));
-        }
+//        // Statuses automatically remove itself once it is finished
+//        // So if MythicalCreature don't have any state anymore, it is time to change state
+//        if (this.statuses().isEmpty()) {
+//            // Loops back to initial state after all state have been transversed
+//            this.statesIndexCounter = (this.statesIndexCounter + 1) % this.statesList.size();
+//            this.addStatus(statesList.get(this.statesIndexCounter));
+//        }
+//    }
     }
 }
