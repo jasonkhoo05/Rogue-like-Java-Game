7905322e4036d724ac65a082f1ee490dec9e464d
Lee Jia Wey <jlee0482@student.monash.edu>
2025-10-01 22:09:19 +0800

    1/10/2025 Created Radiant class for Unicorn

---
+++ src/game/State/Radiant.java
@@ -0,0 +1,14 @@
+package game.State;
+
+
+import edu.monash.fit2099.engine.GameEntity;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.capabilities.Status;
+import edu.monash.fit2099.engine.positions.Exit;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
+import game.Ability;
+
+public class Radiant implements Status {
+
+}
