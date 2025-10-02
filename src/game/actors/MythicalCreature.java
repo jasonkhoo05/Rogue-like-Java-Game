51be9dcc22f6004a41901e9882809f1f6965acf6
Lee Jia Wey <jlee0482@student.monash.edu>
2025-10-02 22:25:53 +0800

    Deleted unnecessary commented lines

--- src/game/actors/MythicalCreature.java
+++ src/game/actors/MythicalCreature.java
@@ -2,6 +2,7 @@ package game.actors;
 
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.capabilities.Status;
+import edu.monash.fit2099.engine.displays.Display;
 import game.State.StatusType;
 import game.capabilities.StateChangeable;
 
@@ -27,14 +28,5 @@ public abstract class MythicalCreature extends Actor implements StateChangeable 
     }
 
     @Override
-    public void changeState(Actor actor) {
-//        // Statuses automatically remove itself once it is finished
-//        // So if MythicalCreature don't have any state anymore, it is time to change state
-//        if (this.statuses().isEmpty()) {
-//            // Loops back to initial state after all state have been transversed
-//            this.statesIndexCounter = (this.statesIndexCounter + 1) % this.statesList.size();
-//            this.addStatus(statesList.get(this.statesIndexCounter));
-//        }
-//    }
-    }
+    public void changeState(Actor actor, Display display){}
 }
