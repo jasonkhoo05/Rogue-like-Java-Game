1cf4f9c6833a20f83bf415322dda4868e1eff527
Lee Jia Wey <jlee0482@student.monash.edu>
2025-10-02 22:26:06 +0800

    Added display parameter in changeState

--- src/game/capabilities/StateChangeable.java
+++ src/game/capabilities/StateChangeable.java
@@ -1,8 +1,9 @@
 package game.capabilities;
 
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.displays.Display;
 
 public interface StateChangeable {
-    void changeState(Actor actor);
+    void changeState(Actor actor, Display display);
 
 }
