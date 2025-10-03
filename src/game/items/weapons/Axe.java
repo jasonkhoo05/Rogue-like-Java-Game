d59ae521e2308d0d04d6b6413a144de3f2e867ff
Yiming Xu <yxuu0272@student.monash.edu>
2025-10-04 02:33:51 +0800

    Implement Axe as a carryable Weapon item

---
+++ src/game/items/weapons/Axe.java
@@ -0,0 +1,39 @@
+package game.items.weapons;
+
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.weapons.Weapon;
+
+import java.util.Random;
+
+/**
+ * Axe (p) - A carryable weapon item.
+ * Deals 15 damage with a 75% chance to hit.
+ */
+public class Axe extends Item implements Weapon {
+    private static final int DAMAGE = 15;
+    private static final int HIT_RATE = 75; // %
+    private final Random rng = new Random();
+
+    public Axe() {
+        // name = "Axe", displayChar = 'p', portable = true
+        super("Axe", 'p', true);
+    }
+
+    @Override
+    public String attack(Actor attacker, Actor target, GameMap map) {
+        // check hit or not
+        if (rng.nextInt(100) >= HIT_RATE) {
+            return attacker + " misses " + target + ".";
+        }
+        // hit then reduce hp
+        target.hurt(DAMAGE);
+        return String.format("%s chops %s for %d damage", attacker, target, DAMAGE);
+    }
+
+    @Override
+    public String toString() {
+        return "Axe";
+    }
+}
