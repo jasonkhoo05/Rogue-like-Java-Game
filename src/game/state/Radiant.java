707aacd26f276f26fa89429e9329ef7dac0bbabe
Lee Jia Wey <jlee0482@student.monash.edu>
2025-10-04 18:23:48 +0800

    Renamed package to small letters

--- src/game/state/Radiant.java
+++ src/game/state/Radiant.java
@@ -1,14 +1,13 @@
-package game.State;
+package game.state;
+
 
 import edu.monash.fit2099.engine.GameEntity;
-import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.capabilities.Status;
 import edu.monash.fit2099.engine.positions.Exit;
 import edu.monash.fit2099.engine.positions.Location;
 import game.Ability;
-import game.actors.Player;
 
-public class Vigor implements Status {
+public class Radiant implements Status {
 
     private int duration;
 
@@ -16,16 +15,21 @@ public class Vigor implements Status {
      * Constructor for Radiant state.
      * @param duration number of turns the state lasts
      */
-    public Vigor(int duration) {
+    public Radiant(int duration) {
         this.duration = duration;
     }
-
     @Override
     public void tickStatus(GameEntity currEntity, Location location) {
-        if (duration > 0) {
-            currEntity.enableAbility(Ability.BOOST_DAMAGE);
-        } else {
-            currEntity.disableAbility(Ability.BOOST_DAMAGE);
+        // Heal any adjacent entities with HEALABLE ability
+        for (Exit exit : location.getExits()) {
+            Location dest = exit.getDestination();
+            if (dest.containsAnActor()) {
+                GameEntity neighbour = dest.getActor();
+
+                if (neighbour.hasAbility(Ability.HEALABLE)) {
+                    neighbour.enableAbility(Ability.RECEIVED_HEAL);
+                }
+            }
         }
         duration--;
     }
