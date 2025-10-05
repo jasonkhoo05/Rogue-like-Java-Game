92e72df9b918e4bfb35832b97c838ed6b7639a5c
Yiming Xu <yxuu0272@student.monash.edu>
2025-10-05 22:17:25 +0800

    feat(items/weapons): add YewberryCoating class causing poison on hit

---
+++ src/game/items/weapons/coating/YewberryCoating.java
@@ -0,0 +1,24 @@
+package game.items.weapons.coating;
+
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.StatusEffects;
+
+/**
+ * Coating made from Yewberry.
+ * When the weapon hits, the target becomes poisoned for 5 turns,
+ * taking 4 damage per turn.
+ */
+public class YewberryCoating implements Coating {
+
+    @Override
+    public String name() {
+        return "Yewberry";
+    }
+
+    @Override
+    public void onHit(Actor attacker, Actor target, GameMap map) {
+        // Trigger poison effect (can be stacked)
+        StatusEffects.addPoison(target, 4, 5);
+    }
+}
