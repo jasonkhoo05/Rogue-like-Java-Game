31617700e62d4afada5f1763c704fda0e2be583b
Yiming Xu <yxuu0272@student.monash.edu>
2025-10-06 00:46:34 +0800

    feat(positions): allow coating weapons with Snow when standing on the snow tiles

--- src/game/positions/Snow.java
+++ src/game/positions/Snow.java
@@ -1,6 +1,9 @@
 package game.positions;
 
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
 
 /**
  * A class representing snow on the ground.
@@ -9,5 +12,20 @@ import edu.monash.fit2099.engine.positions.Ground;
 public class Snow extends Ground {
     public Snow() {
         super('.', "Snow");
+    }
+
+    @Override
+    public ActionList allowableActions(Actor actor, Location location, String direction) {
+        ActionList list = new ActionList();
+
+        // Standing on Snow: Adds a "Snow Coating" menu item for each coatable weapon in your inventory (does not consume the item)
+        for (var a : game.actions.CoatWeaponAction.buildFor(
+                actor,
+                new game.items.weapons.coating.SnowCoating(),
+                null // The source is not an item, so it is not consumed.
+        )) {
+            list.add(a);
+        }
+        return list;
     }
 }
