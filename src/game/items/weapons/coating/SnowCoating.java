8dafe6af8b6275a8e3cbb7652547afafb5d19c11
Yiming Xu <yxuu0272@student.monash.edu>
2025-10-05 23:34:03 +0800

    feat(weapons/coating): add SnowCoating (applies frostbite on hit)

---
+++ src/game/items/weapons/coating/SnowCoating.java
@@ -0,0 +1,17 @@
+package game.items.weapons.coating;
+
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.StatusEffects;
+
+/** Snow coating: on hit, apply frostbite (1 warmth/turn, 3 turns). */
+public class SnowCoating implements Coating {
+    @Override public String name() { return "Snow"; }
+
+    @Override
+    public void onHit(Actor attacker, Actor target, GameMap map) {
+        // Can be stacked
+        StatusEffects.addFrostbite(target, 1, 3);
+    }
+}
+
