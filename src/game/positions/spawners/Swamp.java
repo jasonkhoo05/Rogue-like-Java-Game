57d4c3414b5fdf0c17c5d73d52d1b76c185fa4e5
rphu0004 <rphu0004@student.monash.edu>
2025-10-30 16:39:12 +0800

    feat: remove imports

--- src/game/positions/spawners/Swamp.java
+++ src/game/positions/spawners/Swamp.java
@@ -4,14 +4,12 @@ package game.positions.spawners;
 
 import edu.monash.fit2099.engine.positions.Location;
 import game.spawning.ChanceEachTurnPolicy;
-import game.spawning.newborn.NearbyActorGatedChancePolicy;
 import game.spawning.newborn.PoisonOnSpawnEffect;
 
 /**
  * Swamp '~'
  * Spawns only if: there is at least one actor in surrounding tiles AND a 50% roll succeeds.
  * All newborn animals get Poisoned(10 turns @ 5 dmg/turn).
- * Prints simple messages to the provided logger (Display::println).
  */
 public class Swamp extends SpawnerGround {
     public Swamp() {
