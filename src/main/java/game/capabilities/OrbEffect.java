24cbe487f1551d512abe41e63bd389efd601ba4f
skho0064 <skho0064@student.monash.edu>
2025-11-01 17:26:06 +0800

    Created OrbEffect interface

---
+++ src/main/java/game/capabilities/OrbEffect.java
@@ -0,0 +1,10 @@
+package game.capabilities;
+
+import edu.monash.fit2099.engine.actors.Actor;
+
+/**
+ * Represents magical orbs that have special effects
+ */
+public interface OrbEffect {
+    String applyEffect(Actor actor);
+}
